003bfff6a314278c161cb263aae4dff054b28697
bynumjj <rextater99@gmail.com>
2016-04-06 12:06:09 -0700

    Add intial app.js commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+
+
+// map display section
+var map = function() {
+
+    $("#map").append(googleMap);
+};
+
+map.display();
+//note:  find where map display method is defined
+// end map display section
